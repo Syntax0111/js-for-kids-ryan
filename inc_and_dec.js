e33d6e17d082860b3929e500140d8742540d84d1
Navarro, Jorge <jrnavarroee@gmail.com>
2021-12-20 17:22:14 -0800

    Complete Incrementing and decrementing

--- inc_and_dec.js
+++ inc_and_dec.js
@@ -1,4 +1,13 @@
 // Declare a variable to hold a number
+var reinaSkinPrice = 15;
 
 // Increment the number in the variable by 1 and print the variable to the console
 // Tip: console.log("you message in here");
+console.log(++reinaSkinPrice);
+console.log(reinaSkinPrice);
+
+--reinaSkinPrice;
+--reinaSkinPrice;
+--reinaSkinPrice;
+--reinaSkinPrice;
+console.log(reinaSkinPrice);
