d15de16b1d0431c2229015d7ba34949f6fc09e41
Navarro, Jorge <jrnavarroee@gmail.com>
2021-12-20 17:45:51 -0800

    Complete up to finding the length of a string

---
+++ strings.js
@@ -0,0 +1,20 @@
+var numberNine = 9;
+var stringNine = "9";
+
+console.log(numberNine + numberNine);
+console.log(stringNine + stringNine);
+
+// Challenge
+
+// 1. Declare a variable that contains your first name
+var myName = "Ryan ";
+
+// 2. Declare a variable that contains your last name
+var lastName = "Navarro";
+
+// 3. Join your first and last name and store in a variable called myFullName
+var myFullname = myName + lastName;
+console.log(myFullname);
+
+// 4. How long is your complete name?
+console.log(myFullname.length);
