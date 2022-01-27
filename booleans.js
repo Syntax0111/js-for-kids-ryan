839e26f0e474d3f0f7d9197dcebd081c8aea46b3
Navarro, Jorge <jrnavarroee@gmail.com>
2022-01-26 16:45:09 -0800

    Complete age requirements challenge

--- booleans.js
+++ booleans.js
@@ -1,3 +1,10 @@
 // age verification challenge
-var age = 12;
+var age = 13;
 var accompanied = true;
+
+// var isOverThirteen = age >= 13;
+// var access = isOverThirteen || accompanied;
+
+var access = (age >= 13) || accompanied;
+
+console.log(access);
