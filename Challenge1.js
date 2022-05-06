9a32f562269da1b7daae14a47a7007c25146a87d
Navarro, Jorge <jrnavarroee@gmail.com>
2022-05-06 15:26:18 -0700

    Complete Chapter 4 - Objects. Start challenges

---
+++ Challenge1.js
@@ -0,0 +1,5 @@
+// Create an object and store it in a variable called 'scores'
+
+// Add yourself, and two other friends as keys in the 'scores' object with a starting value of 0
+
+// How would you increase the score
---
+++ objects.js
@@ -0,0 +1,7 @@
+var anna = { name: 'Anna', age: 11, luckyNumbers: [2, 4, 8, 16] };
+var dave = { name: 'Dave', age: 5, luckyNumbers: [3, 9, 40] };
+var kate = { name: 'Kate', age: 9, luckyNumbers: [1, 2, 3] };
+
+var friends = [anna, dave, kate];
+
+console.log(friends[1]);
